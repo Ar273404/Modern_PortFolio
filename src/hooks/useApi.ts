25f451fc334c7aeef4c9603b332160f9de6da42a
Ar273404 <arun273404@gmail.com>
2025-10-31 19:41:14 +0530

    pp

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL =  "http://localhost:8080" ;
+const API_BASE_URL =  "https://modern-portfolio-1-zcnj.onrender.com" ;
 // import.meta.env.VITE_API_URL ||
 
 // Create axios instance with default config
